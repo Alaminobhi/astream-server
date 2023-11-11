be0d10c99eb6714d6b0164e9fc65b477f8c6a60c
Alaminobhi <alaminobhi2@gmail.com>
2023-11-11 21:40:07 +0600

    hlm

--- index.js
+++ index.js
@@ -93,6 +93,53 @@ app.post('/added-stream', async (req, res) => {
   
   });
 
+  app.get('/added-stream', async (req, res) => {
+    const {fileurl, loop, urlkey} =req.body;
+      console.log('huihuygygyug', fileurl, loop, urlkey);
+  
+    //   fs.readFile( __dirname + "/" + "videos/ok.mp4", 'utf8', function (err, data) {
+    //     console.log( data );
+    //     res.end( data );
+    //  });
+     
+  
+    try {
+      const filePath = await path.join(__dirname, './videos/ok.mp4');
+      const videoPath = await "./videos/ok.mp4";
+  
+      const url ='rtmps://live-api-s.facebook.com:443/rtmp/FB-246363178443448-0-AbzNhl9Dkqj2GHtF';
+      const url1 ='FB-231542346605076-0-AbyE4AmCXITp4eKd';
+  
+  
+       const ffmpegProcess = await spawn(ffmpegPath, ['-stream_loop', loop, '-re', '-i', videoPath, 
+          '-c', 'copy',
+          '-f', 'flv', urlkey,]);
+  
+          ffmpegProcess.stdout.on('data', (data) => {
+                console.log("fhuhuh", data.toString());
+                
+              });
+          ffmpegProcess.stderr.on('data', (data) => {
+            
+                console.log(data.toString());
+              });
+          ffmpegProcess.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+          });
+          // Handle errors
+          ffmpegProcess.on('error', (err) => {
+            res.send("spawning ffmpeg", err);
+              console.error(`Error spawning ffmpeg: ${err}`);
+          });
+          res.send({hi: "hhhhhhh hgygygyg hgygyg"});
+    
+     } catch (error) {
+      res.send({hi: "hhhhhhh hgygygyg hgygyg"}, error);
+       console.log(error);
+     }
+    
+    });
+
 const PORT = process.env.PORT || 5000;
 var server = httpServer.listen(PORT, () => {
   
